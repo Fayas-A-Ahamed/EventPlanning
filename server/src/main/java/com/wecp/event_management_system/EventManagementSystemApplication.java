1f55c6b09b4406b7a5a214b461fd3ccae4aa718c
username <wecp-dummy@gmail.com>
2024-08-20 09:29:52 +0000

    wecp initial commit

---
+++ server/src/main/java/com/wecp/event_management_system/EventManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.wecp.event_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class EventManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/com/wecp/event_management_system/entities/Client.java
@@ -0,0 +1,14 @@
+package com.wecp.event_management_system.entities;
+ 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+ 
+@Entity
+@Table(name="clients")
+public class Client extends User {
+
+
+
+
+}
